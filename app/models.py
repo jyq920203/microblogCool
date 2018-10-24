d0b957613fcc9809ecaaf19c2d738cd46ee92601
cojiangyaqi <jyq920203@163.com>
2018-10-24 19:38:36 +0800

    10.9

--- app/models.py
+++ app/models.py
@@ -1,22 +1,17 @@
 from datetime import datetime
-from app import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from app import login,app
 from hashlib import md5
+from flask import current_app
+from app import db,login
 from time import time
 import jwt
 
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-followers = db.Table('followers',
-                     db.Column('follower_id',db.Integer,db.ForeignKey('user.id')),
-                     db.Column('followed_id',db.Integer,db.ForeignKey('user.id'))
-                     )
-
+followers = db.Table(
+    'followers',
+    db.Column('follower_id',db.Integer,db.ForeignKey('user.id')),
+    db.Column('followed_id',db.Integer,db.ForeignKey('user.id'))
+)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,12 +21,24 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash,password)
+
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
 
     def follow(self, user):
         if not self.is_following(user):
@@ -52,32 +59,24 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash,password)
-
-    def avatar(self,size):
-        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
     def get_reset_password_token(self,expires_in=600):
         return jwt.encode(
             {'reset_password': self.id,'exp': time() + expires_in},
-            app.config['SECRET_KEY'],algotithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'],
+            algotithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token,app.config['SECRET_KEY'],
+            id = jwt.decode(token,current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
