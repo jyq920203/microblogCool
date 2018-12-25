798fbe49ae22d6620057aed7eb2ebd1dd1f1491d
cojiangyaqi <jyq920203@163.com>
2018-12-25 17:41:43 +0800

    10.9

--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ from app import db,login
 from time import time
 import jwt
 from app.search import add_to_index, remove_from_index, query_index
+import json
+import redis
+import rq
 
 class SearchableMixin(object):
     @classmethod
@@ -80,6 +83,8 @@ class User(UserMixin, db.Model):
     last_message_read_time = db.Column(db.DateTime)
     notifications = db.relationship('Notification',backref='user',
                                     lazy='dynamic')
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -132,6 +137,30 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
 
+    def add_notification(self,name,data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name,payload_json=json.dumps(data),user=self)
+        db.session.add(n)
+        return n
+
+    def launch_task(self,name,description,*args,**kwargs):
+        rq_job = current_app.task_queue.enqueue('app.tasks.' + name, self.id,
+                                                *args,**kwargs)
+        task = Task(id=rq_job.get_id(), name=name, description=description,
+                    user=self)
+        db.session.add(task)
+        return task
+
+    def get_tasks_in_progress(self):
+        return Task.query.filter_by(user=self,complete=False).all()
+
+    def get_task_in_progress(self,name):
+        return Task.query.filter_by(name=name,user=self,
+                                    complete=False).first()
+
+
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -148,6 +177,7 @@ class Post(SearchableMixin, db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
+
 class Message(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     sender_id = db.Column(db.Integer,db.ForeignKey('user.id'))
@@ -157,3 +187,32 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(128),index=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float,index=True,default=time)
+    payload_json=db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
+
+class Task(db.Model):
+    id = db.Column(db.String(36),primary_key=True)
+    name = db.Column(db.String(128),index=True)
+    description = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    complete = db.Column(db.Boolean,default=False)
+
+    def get_rq_job(self):
+        try:
+            rq_job = rq.job.Job.fetch(self.id,connection=current_app.redis)
+        except (redis.exceptions.RedisError,rq.exception.NoSuchJobError):
+            return None
+        return rq_job
+
+    def get_progress(self):
+        job = self.get_rq_job()
+        return job.meta.get('progress',0) if job is not None else 100
